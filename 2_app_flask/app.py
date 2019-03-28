672e600deb7a19d867c41cae01188d03ddea3c27
Chr1st0f <admin@cyber-secu.com>
2019-03-28 15:54:47 +0100

    Before integration of mysql in flask app

--- 2_app_flask/app.py
+++ 2_app_flask/app.py
@@ -1,4 +1,5 @@
-#!../../venv_flask/bin/python
+#!/opt/rh/rh-python36/root/usr/bin/python
+##!../../venv_flask/bin/python
 # !--*--coding:utf-8--*--
 
 from db.data import Articles # Import data . Should be replaced by a DB
@@ -29,4 +30,4 @@ def page_not_found(error):
 	return render_template('errors/404.html'), 404
 
 if __name__=='__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port='3001', debug=True)
